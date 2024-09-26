8dbca18d353b8fdb71c05e377525ed1299f82a8c
yohannes812 <99536842+yohannes812@users.noreply.github.com>
2024-09-26 16:10:45 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-"""Fetch the total number of subscribers for a given subreddit."""
+""" Export data to CSV from Reddit """
+import json
 import requests
+import sys
 
-def number_of_subscribers(subreddit):
-    """Query the Reddit API to get the subscriber count for a specified subreddit."""
-    user_agent = 'CustomUserAgent/1.0 (by /u/ledbag123)'
-    api_url = f'https://www.reddit.com/r/{subreddit}/about.json'
+def fetch_subscriber_count(subreddit_name):
+    """Fetch the number of subscribers for a given subreddit using the Reddit API."""
+    reddit_user = 'ledbag123'
+    reddit_password = 'Reddit72'
+    credentials = {'username': reddit_user, 'password': reddit_password, 'grant_type': 'password'}
+    request_headers = {'User-Agent': '/u/ledbag123 API Client for Educational Purposes'}
+    api_url = f'https://www.reddit.com/r/{subreddit_name}/about.json'
     
-    headers = {'User-Agent': user_agent}
-    response = requests.get(api_url, headers=headers, allow_redirects=False)
+    session = requests.Session()
+    session.headers.update(request_headers)
     
+    response = session.get(api_url, allow_redirects=False)
     if response.status_code == 200:
         return response.json()["data"]["subscribers"]
     else:
